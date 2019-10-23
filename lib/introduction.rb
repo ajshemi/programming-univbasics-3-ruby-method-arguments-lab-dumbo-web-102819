fa7c69abaaaf11dfb6284112c2fe5e01acff19b1
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-23 03:27:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
-  "Hi, my name is #{}."
+  "Hi, my name is #{name}."
 end
 def introduction_with_language(name,language)
 end
